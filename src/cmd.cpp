2550a01bd4f262c89b98b868c21b93fdf440e6c5
Lt-Henry <quiqueiii@gmail.com>
2017-11-21 16:59:08 +0100

    Added cmd namespace

---
+++ src/cmd.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 Edupals project
+ *
+ * Author:
+ *  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+ *
+ * Source:
+ *  https://github.com/edupals/edupals-base-toolkit
+ *
+ * This file is a part of edupals-base-toolkit.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+ 
+#include "cmd.hpp"
+
+using namespace std;
+
+
